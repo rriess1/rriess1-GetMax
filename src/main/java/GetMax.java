66adad7bd3e1e33671f59ed7121a0b7c0fe896e3
rriess1 <robertwriess@yahoo.com>
2023-02-25 20:59:15 +0000

    second

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,8 +7,8 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        int max = 0;
-        for(int i = 0; i < arr.length; i++){
+        int max = arr[0];
+        for(int i = 1; i < arr.length; i++){
             if(arr[i] > max){
                 max = arr[i];
             }
